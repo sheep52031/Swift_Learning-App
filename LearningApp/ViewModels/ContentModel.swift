45b14a2008e01754cd69f6f0aac0dd51df37ebf0
Buzz <sheep52031@gmil.com>
2022-03-25 17:56:40 +0800

    -rename content view to home view -organize files into folders -add content model set content model as environment object

---
+++ LearningApp/ViewModels/ContentModel.swift
@@ -0,0 +1,17 @@
+//
+//  ContentModel.swift
+//  LearningApp
+//
+//  Created by 林家任 on 2022/3/25.
+//
+
+import Foundation
+
+
+class ContentModel: ObservableObject {
+    
+    
+    
+    
+    
+}
