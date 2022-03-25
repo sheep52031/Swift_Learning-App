be9cb14a5a5847b8bc9dbf80e2c0de366bce7ab2
Buzz <sheep52031@gmil.com>
2022-03-25 17:19:36 +0800

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by 林家任 on 2022/3/25.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
